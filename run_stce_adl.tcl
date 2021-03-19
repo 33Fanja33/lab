6887b5d717ae9856646821bd72efdb81628df179
33Fanja33 <80692517+33Fanja33@users.noreply.github.com>
2021-03-19 10:04:58 +0100

    Add files via upload

---
+++ run_stce_adl.tcl
@@ -0,0 +1,7 @@
+set_family {PolarFire}
+set_proj_path {C:\Future\FPGA\Microsemi\AvalancheBoard\Labs\Lab1\lab1\lab1.prjx}
+set_editor_type {PLACEANDROUTE}
+read_adl {C:\Future\FPGA\Microsemi\AvalancheBoard\Labs\Lab1\lab1\designer\LedBlinkingDSpeed\LedBlinkingDSpeed.adl}
+map_netlist
+read_sdc {C:\Future\FPGA\Microsemi\AvalancheBoard\Labs\Lab1\lab1\constraint\LedBlinkingDSpeed_sdc.sdc}
+set_output_sdc {C:\Future\FPGA\Microsemi\AvalancheBoard\Labs\Lab1\lab1\constraint\user.sdc}
